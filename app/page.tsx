7dbbd87c573950fad2b7c75d37a24243d339fa4b
decryp7 <d3cryp7@gmail.com>
2023-09-20 10:15:48 +0800

    updated. test getting geolocation. need HTTPS to work.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,36 @@
+'use client'
+import {useState, useEffect} from "react";
+
+interface LocationData {
+    latitude: number;
+    longitude: number;
+}
+
+declare type CurrentLocation = LocationData | undefined;
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    </main>
+    const [currentLocation, setCurrentLocation] = useState<CurrentLocation>(undefined);
+
+    useEffect(() => {
+        if('geolocation' in navigator) {
+            // Retrieve latitude & longitude coordinates from `navigator.geolocation` Web API
+            navigator.geolocation.getCurrentPosition(({ coords }) => {
+                const { latitude, longitude } = coords;
+                setCurrentLocation({latitude, longitude});
+            }, error =>{
+                alert(error.message);
+            })
+        }else{
+            alert("geolocation not supported!");
+        }
+    }, []);
+
+    return (
+      <main className="columns-2xs p-2">
+        <div className="min-w-fit w-fit border-2 p-2">
+          <label>Search: </label><input width="200px" />
+        </div>
+        <div className="w-fit border-2 p-2">{currentLocation !== undefined ? JSON.stringify(currentLocation) : "Unknown"}</div>
+      </main>
   )
 }
