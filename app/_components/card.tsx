30c47e3f0634dbe6d8d67ab977c1b651dc244fa2
decryp7 <d3cryp7@gmail.com>
2023-09-26 22:54:09 +0800

    updated

--- app/_components/card.tsx
+++ app/_components/card.tsx
@@ -32,7 +32,7 @@ const Card = React.memo(React.forwardRef((
 
     return <div style={style}
             className={`fixed lg:w-1/4 w-full h-3/4 left-1/2 -translate-x-1/2 bg-white rounded-t-xl shadow-t-xl transition-transform ease-in-out duration-200 will-change-auto`} ref={card}>
-            <div style={{background: `${props.labelColor}`}} className="absolute font-bold w-fit px-2 text-xs rounded-b mr-2 right-0">{props.header}</div>
+            <div style={{background: `${props.labelColor}`}} className="absolute font-bold w-fit px-2 text-xs rounded-b mr-10 right-0">{props.header}</div>
             <button onClick={toggleCard} className="block ml-auto mr-auto mt-2 w-8 h-2 bg-gray-200 rounded-full m-0"></button>
             <div className="p-2 text-black">{props.children}</div>
         </div>
