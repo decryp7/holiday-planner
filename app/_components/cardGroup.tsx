9c94858f58e52f51cd2e9df8ae2719dd1ba7a397
decryp7 <d3cryp7@gmail.com>
2023-09-26 22:52:10 +0800

    investigating how to set ref to child component

--- app/_components/cardGroup.tsx
+++ app/_components/cardGroup.tsx
@@ -1,7 +1,8 @@
-import React, {Children, Fragment, useEffect, useRef, useState} from "react";
+import React, {Children, Fragment, ReactNode, useEffect, useRef, useState} from "react";
 import Card from "@/app/_components/card";
 
 const CardGroup = React.memo((props : {children: any} , context) =>{
+    const childRef = useRef(null);
 
     return <Fragment>{props.children}</Fragment>;
 });
