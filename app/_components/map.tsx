3b578b3bd58fe3443f07cd1372c94da4b2cb1dc9
decryp7 <d3cryp7@gmail.com>
2023-09-27 21:59:13 +0800

    change map marker

--- app/_components/map.tsx
+++ app/_components/map.tsx
@@ -25,12 +25,10 @@ const Map = React.memo((
        {location !== undefined ?
            <MarkerF icon={{
                path:
-                   "M8 12l-4.7023 2.4721.898-5.236L.3916 5.5279l5.2574-.764L8 0l2.3511 4.764 5.2574.7639-3.8043 3.7082.898 5.236z",
-               fillColor: "yellow",
-               fillOpacity: 0.9,
+                   "M6 18L18 6M6 6l12 12",
                scale: 2,
-               strokeColor: "gold",
-               strokeWeight: 2,
+               strokeColor: "Crimson",
+               strokeWeight: 8,
            }} position={location}/>
            : <></>}
       </GoogleMap>) : <></>;
