ef269349e48a337095acfb3b4e399a907fb8fde4
decryp7 <d3cryp7@gmail.com>
2023-10-02 21:51:33 +0800

    update PWA information and tweak PWA settings

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -4,10 +4,10 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-const APP_NAME = "PWA App";
-const APP_DEFAULT_TITLE = "My Awesome PWA App";
-const APP_TITLE_TEMPLATE = "%s - PWA App";
-const APP_DESCRIPTION = "Best PWA app in the world!";
+const APP_NAME = "Taiwan 2023";
+const APP_DEFAULT_TITLE = "Taiwan 2023";
+const APP_TITLE_TEMPLATE = "%s App";
+const APP_DESCRIPTION = "Taiwan 2023";
 
 export const metadata: Metadata = {
   applicationName: APP_NAME,
