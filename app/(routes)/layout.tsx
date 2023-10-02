53dc1bb222fb6d2a7274de0ab2777c3034a5b232
Kok Yong <d3cryp7@gmail.com>
2023-10-02 16:33:16 +0800

    add library for pwa

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -4,10 +4,47 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const APP_NAME = "PWA App";
+const APP_DEFAULT_TITLE = "My Awesome PWA App";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = "Best PWA app in the world!";
+
 export const metadata: Metadata = {
-  title: 'Taiwan 2023',
-  description: 'Taiwan 2023',
-}
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  themeColor: "#FFFFFF",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
 
 export default function RootLayout({
   children,
--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+const {
+    PHASE_DEVELOPMENT_SERVER,
+    PHASE_PRODUCTION_BUILD,
+} = require("next/constants");
+
 const nextConfig = (process.env.CODE_SERVER === undefined) ? {
     output: 'standalone',
 } :
@@ -6,4 +11,12 @@ const nextConfig = (process.env.CODE_SERVER === undefined) ? {
     assetPrefix: '/proxy/3000',
 }
 
-module.exports = nextConfig
+module.exports = (phase) => {
+    if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+        const withPWA = require("@ducanh2912/next-pwa").default({
+            dest: "public",
+        });
+        return withPWA(nextConfig);
+    }
+    return nextConfig;
+};
