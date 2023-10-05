60bde565c771bd7d82080abd540334f28e2ee30c
decryp7 <d3cryp7@gmail.com>
2023-10-05 16:16:17 +0800

    adjust appearance

--- app/_components/weatherMarker.tsx
+++ app/_components/weatherMarker.tsx
@@ -30,14 +30,14 @@ const WeatherMarker = React.memo((props : {
                 onClick={handleClick} ref={weatherMarkerRef}>
         <div className="relative w-[90%] h-[90%] left-1/2 -translate-x-1/2">
             <Image
-                className="drop-shadow-[0px_0px_2px_white]"
+                className=""
                 src={`https://www.cwa.gov.tw/V8/assets/img/weather_icons/weathers/svg_icon/${now}/${props.icon}.svg`}
                 alt={props.value}
                 fill={true}
                 style={{objectFit:"contain"}}/>
             <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-[1em] text-black font-black">{props.temperature}</div>
         </div>
-        <div className="text-[1em] mx-auto text-black font-black drop-shadow-[0px_0px_2px_white]">{props.value}</div>
+        <div className="text-[1em] mx-auto text-black font-black">{props.value}</div>
     </div>
 });
 
