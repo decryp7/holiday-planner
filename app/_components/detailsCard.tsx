03d6855242cfc943cef3ef586d2b2421ee377782
decryp7 <d3cryp7@gmail.com>
2023-10-08 13:46:18 +0800

    fix antipattern of defining component in component which cause remounting

--- app/_components/detailsCard.tsx
+++ app/_components/detailsCard.tsx
@@ -8,72 +8,57 @@ import {DateTime} from "luxon";
 import LoadingSkeleton from "@/app/_components/loadingSkeleton";
 import PhotoCarousel from "@/app/_components/photoCarousel";
 
+async function Details(props: {placeName: string}){
+    if(props.placeName === undefined){
+        return <></>;
+    }
+
+    const url = `/api/places/name?${props.placeName}`;
+    const places = await fetch(url)
+        .then(async res => plainToInstance(Place, await res.json()));
+    if(places.length < 1 || places[0] === undefined){
+        return <></>;
+    }
+    const place = places[0];
+    const openingHours = place.getOpeningHours();
+    const weekday = DateTime.now().weekday;
+
+    return <div className="flex flex-col space-y-2 w-full h-full">
+        <div className="flex flex-row space-x-2">
+            <Title className="text-2xl">{place.name}</Title>
+            <Badge color={place.IsOpen() ? "green" : "red"}>{place.IsOpen() ? "Open" : "Closed"}</Badge>
+        </div>
+        <Text><Italic>{place.address}</Italic></Text>
+        <div className="flex flex-row flex-wrap gap-2">
+            {place.tags.map((tag, index) =>
+                <Badge key={index}>{tag.toString()}</Badge>
+            )}
+        </div>
+        <Divider />
+        {place.description === undefined ? <></> :
+            <Text>{place.description}</Text>}
+        <PhotoCarousel placeName={place.name} placeId={place.gplaceid}/>
+        <Card className="bg-gray-50 p-3">
+            <Title className="px-2">Opening Hours</Title>
+            <div className="flex flex-row flex-wrap gap-2">
+                {openingHours.map((oh, index) =>
+                    <div key={index}>
+                        <Subtitle
+                            className={`w-fit px-2 ${+Weekday[oh.weekday as keyof typeof Weekday]=== weekday ? "text-gray-800 bg-yellow-300" : ""}`}>{oh.weekday}</Subtitle>
+                        <Text
+                            className={`w-fit px-2 ${+Weekday[oh.weekday as keyof typeof Weekday]=== weekday ? "text-gray-800 bg-yellow-300" : ""}`}>{oh.time}</Text>
+                    </div>)}
+            </div>
+        </Card>
+    </div>;
+}
+
 const DetailsCard = React.memo((props : {} , context) =>{
     const selectedMarker = useRecoilValue(selectedMarkerState);
-    const [place, setPlace] = useState<Place>();
-
-    async function fetchDetails(placeName: string){
-        const url = `/api/places/name?${placeName}`;
-        const places = await fetch(url)
-            .then(async res => plainToInstance(Place, await res.json()));
-        if(places.length < 1){
-            return;
-        }
-        setPlace(places[0]);
-    }
-
-    useEffect(() => {
-        if(selectedMarker === undefined){
-            return;
-        }
-
-        fetchDetails(selectedMarker)
-            .catch((e: any) =>{
-                console.log(`Error occurred when fetching details for ${selectedMarker}. Error: ${e}`);
-            });
-    }, [selectedMarker]);
-
-    async function Details(){
-        if(place === undefined){
-            return <></>;
-        }
-
-        const openingHours = place.getOpeningHours();
-        const weekday = DateTime.now().weekday;
-
-        return <div className="flex flex-col space-y-2 w-full h-full">
-            <div className="flex flex-row space-x-2">
-                <Title className="text-2xl">{place.name}</Title>
-                <Badge color={place.IsOpen() ? "green" : "red"}>{place.IsOpen() ? "Open" : "Closed"}</Badge>
-            </div>
-            <Text><Italic>{place.address}</Italic></Text>
-            <div className="flex flex-row flex-wrap gap-2">
-                {place.tags.map((tag, index) =>
-                    <Badge key={index}>{tag.toString()}</Badge>
-                )}
-            </div>
-            <Divider />
-            {place.description === undefined ? <></> :
-                <Text>{place.description}</Text>}
-            <PhotoCarousel placeName={place.name} placeId={place.gplaceid}/>
-            <Card className="bg-gray-50 p-3">
-                <Title className="px-2">Opening Hours</Title>
-                <div className="flex flex-row flex-wrap gap-2">
-                    {openingHours.map((oh, index) =>
-                        <div key={index}>
-                            <Subtitle
-                                className={`w-fit px-2 ${+Weekday[oh.weekday as keyof typeof Weekday]=== weekday ? "text-gray-800 bg-yellow-300" : ""}`}>{oh.weekday}</Subtitle>
-                            <Text
-                                className={`w-fit px-2 ${+Weekday[oh.weekday as keyof typeof Weekday]=== weekday ? "text-gray-800 bg-yellow-300" : ""}`}>{oh.time}</Text>
-                        </div>)}
-                </div>
-            </Card>
-        </div>;
-    }
 
     return <div className="w-full">
         <Suspense fallback={<LoadingSkeleton/>}>
-            <Details />
+            {selectedMarker !== undefined && <Details placeName={selectedMarker} />}
         </Suspense>
     </div>
 });
--- app/_components/weatherCard.tsx
+++ app/_components/weatherCard.tsx
@@ -9,84 +9,67 @@ import {DateTime} from "luxon";
 import Image from "next/image";
 import LoadingSkeleton from "@/app/_components/loadingSkeleton";
 
-const WeatherCard = React.memo((props : {} , context) =>{
-    const selectedWeatherMarker = useRecoilValue(selectedWeatherMarkerState);
-    const [locationForecast, setLocationForecast] = useState<LocationForecast>();
-
-    async function fetchLocationForecast(location: string){
-        const url = `/api/weather/location?${location}`;
-        const fc = await fetch(url)
-            .then(res => res.json()) as LocationForecast;
-        setLocationForecast(fc);
+async function WeatherDetails(props: {location: string}){
+    if(props.location === undefined){
+        return <></>;
     }
 
-    useEffect(() => {
-        if(selectedWeatherMarker === undefined){
-            return;
+    const url = `/api/weather/location?${props.location}`;
+    const locationForecast = await fetch(url)
+        .then(res => res.json()) as LocationForecast;
+
+    const weatherForecastInfos: WeatherForecastInfo[] = [];
+    const temperatureForecastInfos: TemperatureForecastInfo[] = [];
+
+    for(const forecast of locationForecast.forecast){
+        if('icon' in forecast){
+            weatherForecastInfos.push(plainToInstance(WeatherForecastInfo, forecast));
+        }else{
+            temperatureForecastInfos.push(plainToInstance(TemperatureForecastInfo, forecast));
         }
+    }
 
-        fetchLocationForecast(selectedWeatherMarker)
-            .catch((e: any) =>{
-                console.log(`Error occurred when fetching location forecast for ${selectedWeatherMarker}. Error: ${e}`);
-            });
-    }, [selectedWeatherMarker]);
-
-    async function WeatherDetails() {
-        if(locationForecast === undefined){
-            return <></>;
+    const forecastInfos: {weather: WeatherForecastInfo, temperature: TemperatureForecastInfo}[] = [];
+    for(let i=0;i<weatherForecastInfos.length;i++){
+        const temperatureForecast = temperatureForecastInfos[i];
+        if(temperatureForecast != undefined){
+            forecastInfos.push({ weather: weatherForecastInfos[i], temperature: temperatureForecast})
         }
+    }
 
-        console.log(locationForecast.locationName);
+    const currentHour = DateTime.now().hour;
+    const now = currentHour > 7 && currentHour < 19 ? 'day' : 'night';
 
-        const weatherForecastInfos: WeatherForecastInfo[] = [];
-        const temperatureForecastInfos: TemperatureForecastInfo[] = [];
-
-        for(const forecast of locationForecast.forecast){
-            if('icon' in forecast){
-                weatherForecastInfos.push(plainToInstance(WeatherForecastInfo, forecast));
-            }else{
-                temperatureForecastInfos.push(plainToInstance(TemperatureForecastInfo, forecast));
-            }
-        }
-
-        const forecastInfos: {weather: WeatherForecastInfo, temperature: TemperatureForecastInfo}[] = [];
-        for(let i=0;i<weatherForecastInfos.length;i++){
-            const temperatureForecast = temperatureForecastInfos[i];
-            if(temperatureForecast != undefined){
-                forecastInfos.push({ weather: weatherForecastInfos[i], temperature: temperatureForecast})
-            }
-        }
-
-        const currentHour = DateTime.now().hour;
-        const now = currentHour > 7 && currentHour < 19 ? 'day' : 'night';
-
-        return <>
-            <Title className="text-2xl">{locationForecast.locationName}</Title>
-            <div className="flex flex-col md:grid md:grid-cols-2 gap-5">
+    return <>
+        <Title className="text-2xl">{locationForecast.locationName}</Title>
+        <div className="flex flex-col md:grid md:grid-cols-2 gap-5">
             {forecastInfos.map((forecastInfo, index)=>
                 <Card key={index} className="bg-gray-50">
                     <div className="grid grid-cols-3 gap-5">
-                            <Image
-                                className="w-full h-full col-span-1"
-                                src={`https://www.cwa.gov.tw/V8/assets/img/weather_icons/weathers/svg_icon/${now}/${forecastInfo.weather.icon}.svg`}
-                                alt={forecastInfo.weather.value}
-                                width="0"
-                                height="0"
-                                sizes="100vh" />
+                        <Image
+                            className="w-full h-full col-span-1"
+                            src={`https://www.cwa.gov.tw/V8/assets/img/weather_icons/weathers/svg_icon/${now}/${forecastInfo.weather.icon}.svg`}
+                            alt={forecastInfo.weather.value}
+                            width="0"
+                            height="0"
+                            sizes="100vh" />
                         <div className="col-span-2">
                             <Title className="">{forecastInfo.weather.startTime.toLocaleString(DateTimeFormat)}</Title>
                             <Metric className="">{`${forecastInfo.weather.value} (${forecastInfo.temperature.toString()})`}</Metric>
                         </div>
                     </div>
                 </Card>
-                )}
-            </div>
-        </>
-    }
+            )}
+        </div>
+    </>
+}
+
+const WeatherCard = React.memo((props : {} , context) =>{
+    const selectedWeatherMarker = useRecoilValue(selectedWeatherMarkerState);
 
     return <div className="w-full">
         <Suspense fallback={<LoadingSkeleton/>}>
-            <WeatherDetails/>
+            {selectedWeatherMarker !== undefined && <WeatherDetails location={selectedWeatherMarker!}/>}
         </Suspense>
         </div>
 });
