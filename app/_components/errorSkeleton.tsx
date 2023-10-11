a1b624a7354b6168ba8ceb2d05d291937c8dff0c
Kok Yong <d3cryp7@gmail.com>
2023-10-11 14:13:29 +0800

    fix error

--- app/_components/errorSkeleton.tsx
+++ app/_components/errorSkeleton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ErrorSkeleton = React.memo((
     props : {message: string} , context) =>{
-    return <div className="w-full h-screen flex items-center justify-center">
+    return <div className="w-full h-full flex items-center justify-center">
         <span className="text-gray-800 text-2xl">{props.message}</span>
     </div>;
 
